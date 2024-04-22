6abe45120aa2e29e113264c4cff1fc1e70d37915
timdiland <ruslankobrin@gmail.com>
2024-04-22 03:38:49 +0300

    delete custom_filters

--- lib_space/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from datetime import timedelta
-
-register = template.Library()
-
-
-@register.filter
-def add_days(value, days):
-    return value + timedelta(days=days)
